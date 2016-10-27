3d579e05d0964dc632685a0eaf190b45311a6bde
reefer <kazakovs@gmail.com>
2016-10-28 01:21:52 +0200

    Added demo

--- src/main/java/net/kazakovs/TuringMachine.java
+++ src/main/java/net/kazakovs/TuringMachine.java
@@ -3,7 +3,17 @@ package net.kazakovs;
 public class TuringMachine{
 
     public static void main(String[] args) {
-        System.out.println("Hello Gradle!");
+
+        Machine<String> machine = new Machine<>(
+                Utils.readFileInputStreamFromResources("net/kazakovs/rules.txt"),
+                " ",
+                new String[]{"1", "a", "1", "a", "a", "1"} );
+        System.out.println(machine.printTape());
+        while(machine.getCurrentState()!=0){
+            machine.updateState();
+            System.out.println(machine.printTape());
+        }
+
     }
 
 }
